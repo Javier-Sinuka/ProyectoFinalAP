3181005890ad79df3914f3ca1cc23d222a72457d
Javier_Sinuka <javier_sinuka@mi.unc.edu.ar>
2022-12-10 19:33:20 -0300

    Carga de archivos primaria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Pagina404Component} from "./components/pagina404/pagina404.component";
+import {HomeComponent} from "./components/home/home.component";
+import {LoguinComponent} from "./components/loguin/loguin.component";
+import {AboutMeComponent} from "./components/about-me/about-me.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: '/home', pathMatch: "full"},
+  {path:'***', component:Pagina404Component},
+  {path:'home', component:HomeComponent},
+  {path:'loguin', component:LoguinComponent},
+  {path:'about-me', component:AboutMeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoguinComponent } from './components/loguin/loguin.component';
+import { Pagina404Component } from './components/pagina404/pagina404.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoguinComponent,
+    Pagina404Component,
+    HomeComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
